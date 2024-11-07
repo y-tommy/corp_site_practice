c9b322123b396ced3714a394b91bba9f029ee744
y-tommy <yuji.tomie@gmail.com>
2024-11-07 11:45:31 +0900

    Create FAQ Content

---
+++ app/contact/FAQ/FAQItems.tsx
@@ -0,0 +1,5 @@
+export const FAQItems = [
+  { id: "item-1", title: "質問その1", content: "答えその1" },
+  { id: "item-2", title: "質問その2", content: "答えその2" },
+  { id: "item-3", title: "質問その3", content: "答えその3" }
+];
