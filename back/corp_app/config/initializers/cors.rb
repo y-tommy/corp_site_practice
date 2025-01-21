1d7fec2c143ba32cd8c4f95ae48d9340f86215c7
y-tommy <yuji.tomie@gmail.com>
2025-01-21 22:21:07 +0900

    setting cors

--- back/corp_app/config/initializers/cors.rb
+++ back/corp_app/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    # origins "http://localhost:3000"
+    origins ENV['WEBSITE_URL']
 
     resource "*",
       headers: :any,
