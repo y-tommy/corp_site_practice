442a636ebc00947d91df02177401a68966ba2926
y-tommy <yuji.tomie@gmail.com>
2025-01-25 17:00:13 +0900

    fix db

--- back/corp_app/bin/render-build.sh
+++ back/corp_app/bin/render-build.sh
@@ -1,7 +1,7 @@
 set -o errexit
 
 bundle install
-RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop
-RAILS_ENV=production bundle exec rake db:create
-RAILS_ENV=production bundle exec rake db:seed
-RAILS_ENV=production bundle exec rake db:migrate
+# RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop
+bundle exec rake db:create RAILS_ENV=production
+bundle exec rake db:seed RAILS_ENV=production
+bundle exec rake db:migrate RAILS_ENV=production
